4d667493fc5b05f1cd6eb6683a2f11e454fdb9d8
Estavillo <fernandoestavillo@gmail.com>
2025-03-11 01:03:45 -0600

    fix and remove unused pages, update schema

--- yapp-next/src/app/page.js
+++ yapp-next/src/app/page.js
@@ -1,29 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import Establishments from "./establishments/page";
 
-export default function Home() {
+export default function Home({ children }) {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.logo}>YAPP</h1>
+          <h1 className={styles.logo}>YAPP</h1>
       </header>
-      <div className={styles.content}>
-        <nav className={styles.sideMenu}>
-          <ul>
-            <li>Menu Item 1</li>
-            <li>Menu Item 2</li>
-            <li>Menu Item 3</li>
-            <li>
-              <Link href="/establishments">Establishments</Link>
-            </li>
-          </ul>
-        </nav>
-        <main className={styles.main}>
-          <Establishments />
-        </main>
-      </div>
     </div>
   );
 }
